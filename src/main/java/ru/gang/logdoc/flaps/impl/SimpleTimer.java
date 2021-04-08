681ceb6f778541795a3d5e706c07ae9fc9a534bf
dvdanilin <denis@danilin.name>
2021-04-08 09:31:50 +0300

    v0.0.45 переход на след. версию logdoc-structs

--- src/main/java/ru/gang/logdoc/flaps/impl/SimpleTimer.java
+++ src/main/java/ru/gang/logdoc/flaps/impl/SimpleTimer.java
@@ -6,7 +6,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
 
 import static ru.gang.logdoc.structs.utils.Tools.logTimeFormat;
 
@@ -20,28 +19,6 @@ public class SimpleTimer implements Timer {
 
     @Override
     public String apply(final Long timestamp) {
-        final LocalDateTime ldt = Instant.ofEpochMilli(timestamp).atZone(zone).toLocalDateTime();
-
-        return String.valueOf(ldt.getYear()).substring(2)
-                + pad(ldt.getMonthValue(), 10)
-                + pad(ldt.getDayOfMonth(), 10)
-                + pad(ldt.getHour(), 10)
-                + pad(ldt.getMinute(), 10)
-                + pad(ldt.getSecond(), 10)
-                + pad(ldt.get(ChronoField.MILLI_OF_SECOND), 1000)
-                + pad(ldt.get(ChronoField.NANO_OF_SECOND), 1000);
-    }
-
-    private String pad(final int i, final int rad) {
-        if (i >= rad)
-            return String.valueOf(i);
-
-        if (i == 0)
-            return "0" + String.valueOf(rad).substring(1);
-
-        if (rad == 10)
-            return "0" + i;
-
-        return (i < 10 ? "00" : i < 100 ? "0" : "0") + i;
+        return Instant.ofEpochMilli(timestamp).atZone(zone).toLocalDateTime().format(logTimeFormat);
     }
 }
